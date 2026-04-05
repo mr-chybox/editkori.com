0e5bc71deb83bfd9ec5b7bb96df3fe93bb11e647
mr-chybox <mirajulislamofficiale@gmail.com>
2026-04-05 10:09:51 +0600

    update files

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
-const express = require('express');
-const path    = require('path');
-const app     = express();
+const express=require('express'),path=require('path'),app=express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/',      (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
-app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'public', 'admin.html')));
-app.listen(process.env.PORT || 3000, () => console.log('🎬 EditKori running'));
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/',(q,r)=>r.sendFile(path.join(__dirname,'public','index.html')));
+app.get('/admin',(q,r)=>r.sendFile(path.join(__dirname,'public','admin.html')));
+app.listen(process.env.PORT||3000,()=>console.log('🎬 EditKori running'));
